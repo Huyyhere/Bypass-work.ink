1c3956a74e43e7b68bcf2d7181aa95db5aa93fd2
Huyyhere <bquangghuyy2011@gmail.com>
2025-10-05 22:14:46 +0700

    Create bypass-work-ink.js

---
+++ bypass-work-ink.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Bypass Work.ink Huyyhere
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Ultimate bypasses work.ink
+// @author       huyyhere
+// @match        https://work.ink/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=work.ink
+// @run-at       document-start
+// @license      MIT
+// @updateURL    https://raw.githubusercontent.com/Huyyhere/Bypass-work.ink/refs/heads/main/scripts.js
+// @downloadURL  https://raw.githubusercontent.com/Huyyhere/Bypass-work.ink/refs/heads/main/scripts.js
+// @require      https://raw.githubusercontent.com/Huyyhere/Bypass-work.ink/refs/heads/main/scripts.js
+// ==/UserScript==
