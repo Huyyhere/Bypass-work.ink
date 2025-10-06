6bef7f62b9b7d0011de8b322867ffb889b6c3fff
Huyyhere <bquangghuyy2011@gmail.com>
2025-10-06 11:56:31 +0700

    Update bypass-workink-user.js

--- bypass-workink-user.js
+++ bypass-workink-user.js
@@ -2,18 +2,13 @@
 // @name         Bypass Work.ink Huyyhere
 // @namespace    http://tampermonkey.net/
 // @version      1.0.0
-// @description  Ultimate bypasses work.ink with auto-update check
+// @description  Ultimate bypasses work.ink
 // @author       huyyhere
-// @match        https://*.work.ink/*
-// @match        https://workink.click/*
-// @match        *://*/direct/?*
+// @match        https://work.ink/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=work.ink
 // @run-at       document-start
 // @license      MIT
 // @updateURL    https://raw.githubusercontent.com/Huyyhere/Bypass-work.ink/refs/heads/main/scripts.js
 // @downloadURL  https://raw.githubusercontent.com/Huyyhere/Bypass-work.ink/refs/heads/main/scripts.js
-// @grant        GM_xmlhttpRequest
-// @grant        GM_getValue
-// @grant        GM_setValue
-// @grant        GM_addStyle
+// @require      https://raw.githubusercontent.com/Huyyhere/Bypass-work.ink/refs/heads/main/scripts.js
 // ==/UserScript==
