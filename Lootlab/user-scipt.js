5c08c920c788f13181ce01a12b6c2ceae399ed37
Huyyhere <bquangghuyy2011@gmail.com>
2025-10-07 23:52:31 +0700

    Update user-scipt.js

--- Lootlab/user-scipt.js
+++ Lootlab/user-scipt.js
@@ -1,1 +1,14 @@
-Comming Soon!
+// ==UserScript==
+// @name         Bypass Work.ink
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Ultimate bypasses work.ink
+// @author       huyyhere
+// @match        https://work.ink/*
+// @icon         https://bom.edu.vn/public/upload/2024/12/capybara-meme-chibi-6.webp
+// @run-at       document-start
+// @license      MIT
+// @updateURL    https://raw.githubusercontent.com/Huyyhere/Bypass-link/refs/heads/main/Lootlabs/script.js
+// @downloadURL  https://raw.githubusercontent.com/Huyyhere/Bypass-link/refs/heads/main/Lootlabs/script.js
+// @require      https://raw.githubusercontent.com/Huyyhere/Bypass-link/refs/heads/main/Lootlabs/script.js
+// ==/UserScript==
