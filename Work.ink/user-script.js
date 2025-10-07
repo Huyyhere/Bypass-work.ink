5a93a59236cef75466a30c14a04caabe953a4c05
Huyyhere <bquangghuyy2011@gmail.com>
2025-10-07 10:03:00 +0700

    Update user-script.js

--- Work.ink/user-script.js
+++ Work.ink/user-script.js
@@ -1,15 +1,14 @@
 // ==UserScript==
-// @name         Bypass Volcano Executor
+// @name         Bypass Work.ink
 // @namespace    http://tampermonkey.net/
-// @version      1.0.5
-// @description  Ultimate bypasses volcano
+// @version      1.0.0
+// @description  Ultimate bypasses work.ink
 // @author       huyyhere (Copyright source IHaxU)
 // @match        https://work.ink/*
-// @match        https://key.volcano.wtf/*
 // @icon         https://bom.edu.vn/public/upload/2024/12/capybara-meme-chibi-6.webp
 // @run-at       document-start
 // @license      MIT
-// @updateURL    https://raw.githubusercontent.com/Huyyhere/Bypass-link/refs/heads/main/Volcano/script.js
-// @downloadURL  https://raw.githubusercontent.com/Huyyhere/Bypass-link/refs/heads/main/Volcano/script.js
-// @require      https://raw.githubusercontent.com/Huyyhere/Bypass-link/refs/heads/main/Volcano/script.js
+// @updateURL    https://raw.githubusercontent.com/Huyyhere/Bypass-link/refs/heads/main/Work.ink/script.js
+// @downloadURL  https://raw.githubusercontent.com/Huyyhere/Bypass-link/refs/heads/main/Work.ink/script.js
+// @require      https://raw.githubusercontent.com/Huyyhere/Bypass-link/refs/heads/main/Work.ink/script.js
 // ==/UserScript==
