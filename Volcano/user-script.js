595bd33cdfb6b3c86e09e58926c77b2490882577
Huyyhere <bquangghuyy2011@gmail.com>
2025-10-07 10:15:16 +0700

    Update user-script.js

--- Volcano/user-script.js
+++ Volcano/user-script.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0.5
 // @description  Ultimate bypasses volcano
-// @author       huyyhere (Copyright source IHaxU)
+// @author       huyyhere
 // @match        https://work.ink/*
 // @match        https://key.volcano.wtf/*
 // @icon         https://bom.edu.vn/public/upload/2024/12/capybara-meme-chibi-6.webp
