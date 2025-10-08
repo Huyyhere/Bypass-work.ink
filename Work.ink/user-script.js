ca667a417ee151af2ec37e504be0313cd96dc804
Huyyhere <bquangghuyy2011@gmail.com>
2025-10-08 10:08:21 +0700

    Update user-script.js

--- Work.ink/user-script.js
+++ Work.ink/user-script.js
@@ -1,1 +1,15 @@
-
+// ==UserScript==
+// @name         Bypass Work.ink
+// @namespace    http://tampermonkey.net/
+// @version      1.5.0
+// @description  Auto bypass Work.ink [8/10/2025-VN]
+// @author       huyyhere
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=key.volcano.wtf
+// @match        https://work.ink/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=work.ink
+// @run-at       document-start
+// @license      MIT
+// @updateURL    https://raw.githubusercontent.com/Huyyhere/Bypass-work.ink/refs/heads/main/scripts.js
+// @downloadURL  https://raw.githubusercontent.com/Huyyhere/Bypass-work.ink/refs/heads/main/scripts.js
+// @require      https://raw.githubusercontent.com/Huyyhere/Bypass-work.ink/refs/heads/main/scripts.js
+// ==/UserScript==
