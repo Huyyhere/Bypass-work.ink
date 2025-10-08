fe156b3649a545299f1292c41f19ef7ae04c3a45
Huyyhere <bquangghuyy2011@gmail.com>
2025-10-08 10:56:50 +0700

    Update user-script.js

--- Work.ink/user-script.js
+++ Work.ink/user-script.js
@@ -4,7 +4,7 @@
 // @version      1.5.0
 // @description  Auto bypass Work.ink [8/10/2025-VN]
 // @author       huyyhere
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=key.volcano.wtf
+// @icon         https://cdn-media.sforum.vn/storage/app/media/anhthem/meme-tinh-yeu-61.jpg
 // @match        https://work.ink/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=work.ink
 // @run-at       document-start
